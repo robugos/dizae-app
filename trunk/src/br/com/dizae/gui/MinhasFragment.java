ffaeed5b42ae04308cb2a5c5171207175d2304cc
ulrichugo@gmail.com <ulrichugo@gmail.com@f15822e0-2de8-6bd2-28df-9d8f6187d55a>
2014-12-15 13:33:24 +0000

    First commit

---
+++ trunk/src/br/com/dizae/gui/MinhasFragment.java
@@ -0,0 +1,33 @@
+package br.com.dizae.gui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import br.com.dizae.R;
+
+public class MinhasFragment extends Fragment {
+	
+	Button mostrarOcorrencias;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		
+		View principal = inflater.inflate(R.layout.fragment_minhas, container,
+				false);
+		
+		mostrarOcorrencias = (Button) principal.findViewById(R.id.allOcorrencias);
+		mostrarOcorrencias.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				startActivity(new Intent(getActivity(),	AllOcorrenciasActivity.class));
+			}
+		});
+
+		return principal;
+	}
+}
