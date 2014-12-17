5b81d3a481053fc78b84795b53560bdf5ddd1b95
blendaguedes@gmail.com <blendaguedes@gmail.com@f15822e0-2de8-6bd2-28df-9d8f6187d55a>
2014-12-17 05:49:35 +0000

    Tarefas da Sprint 2,3 e uma da 4 por Blenda

--- trunk/gen/br/com/dizae/R.java
+++ trunk/gen/br/com/dizae/R.java
@@ -28,24 +28,36 @@ public final class R {
         public static final int splashscreen=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
-        public static final int allOcorrencias=0x7f080002;
-        public static final int favoritar=0x7f080003;
-        public static final int listviewPredio=0x7f080001;
-        public static final int name=0x7f080006;
-        public static final int nome=0x7f080004;
-        public static final int pid=0x7f080007;
-        public static final int quantAndar=0x7f080005;
-        public static final int rolagem=0x7f080000;
+        public static final int action_settings=0x7f080012;
+        public static final int allOcorrencias=0x7f08000c;
+        public static final int button1=0x7f080009;
+        public static final int editText1=0x7f080007;
+        public static final int editText2=0x7f080004;
+        public static final int editText3=0x7f080002;
+        public static final int editText4=0x7f080006;
+        public static final int favoritar=0x7f08000d;
+        public static final int listviewPredio=0x7f08000b;
+        public static final int name=0x7f080010;
+        public static final int nome=0x7f08000e;
+        public static final int pid=0x7f080011;
+        public static final int quantAndar=0x7f08000f;
+        public static final int rolagem=0x7f08000a;
+        public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080003;
+        public static final int textView3=0x7f080001;
+        public static final int textView4=0x7f080008;
+        public static final int textView5=0x7f080005;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_splash=0x7f030001;
-        public static final int fragment_apoiada=0x7f030002;
-        public static final int fragment_minhas=0x7f030003;
-        public static final int listview_apoiada=0x7f030004;
-        public static final int listview_minhas=0x7f030005;
-        public static final int minhas_ocorrencias=0x7f030006;
+        public static final int activity_gerar_ocorrencia=0x7f030000;
+        public static final int activity_login=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_splash=0x7f030003;
+        public static final int fragment_apoiada=0x7f030004;
+        public static final int fragment_minhas=0x7f030005;
+        public static final int listview_apoiada=0x7f030006;
+        public static final int listview_minhas=0x7f030007;
+        public static final int minhas_ocorrencias=0x7f030008;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -54,7 +66,10 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int carregando=0x7f050003;
+        public static final int hello_world=0x7f050006;
         public static final int site=0x7f050002;
+        public static final int title_activity_gerar_ocorrencia=0x7f050007;
+        public static final int title_activity_login=0x7f050005;
         public static final int title_ocorrencias_minhas=0x7f050004;
     }
     public static final class style {
